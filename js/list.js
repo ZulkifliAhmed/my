9324bf02767c45ae887b7cd90c0225e6567139e4
Zulkifli-ذوالكفل <136604803+ZulkifliAhmed@users.noreply.github.com>
2025-06-06 23:53:20 +0300

    Add files via upload

--- js/list.js
+++ js/list.js
@@ -1,32 +1,20 @@
 let allAudio = [
-    {
-        name : "al-fatiha",
-        artist : "abdul-rahman-al-sudais",
-        img : "images/img-1.jpg",
-        src : "audio/al-fatiha.mp3"
-    },
-      {
-        name : "al-baqara",
-        artist : "sabdul-rahman-al-sudais",
-        img : "images/img-2.jpg",
-        src : "audio/al-baqara.mp3"
-    },
-      {
-        name : "ya-seen",
-        artist : "abdul-rahman-al-sudais",
-        img : "images/img-3.jpg",
-        src : "audio/ya-seen.mp3"
-    },
-     {
-        name : "an-noor",
-        artist : "abdul-rahman-al-sudais",
-        img : "images/img-4.jpg",
-        src : "audio/an-noor.mp3"
-    },
-     {
-        name : "al-kahf",
-        artist : "abdul-rahman-al-sudais",
-        img : "images/img-5.png",
-        src : "audio/al-kahf.mp3"
-    },
-]
+  {
+    name: "al-fatiha",
+    artist: "abdul-rahman-al-sudais",
+    img: "images/img-1.jpg",
+    src: "audio/al-fatiha.mp3",
+  },
+  {
+    name: "ya-seen",
+    artist: "abdul-rahman-al-sudais",
+    img: "images/img-3.jpg",
+    src: "audio/ya-seen.mp3",
+  },
+  {
+    name: "an-noor",
+    artist: "abdul-rahman-al-sudais",
+    img: "images/img-4.jpg",
+    src: "audio/an-noor.mp3",
+  },
+];
